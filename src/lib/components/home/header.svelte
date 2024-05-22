386cdc690a31ecad87de7fc845842cbef149e436
Ahmed Azmy <ahmedazmycs@protonmail.com>
2024-05-22 20:23:19 +0300

    homepage header

--- src/lib/components/home/header.svelte
+++ src/lib/components/home/header.svelte
@@ -1,12 +1,19 @@
 <script>
-	import bg from '$assets/imgs/header_bg2.png';
+	import bg from '$assets/imgs/header_bg2.png?enhanced';
 </script>
 
-<div class="h-[770px] relative bg-[#EAF1FF] overflow-hidden flex justify-center" dir="rtl">
-	<img src={bg} alt="bg" class="lg:w-[38vw] select-none absolute left-0 object-cover h-full" />
+<div
+	class="h-[770px] relative bg-gradient-to-b from-theme-skyblue to-white overflow-hidden flex justify-center"
+	dir="rtl"
+>
+	<enhanced:img
+		src={bg}
+		alt="bg"
+		class="lg:w-[38vw] select-none absolute left-0 object-cover h-full"
+	/>
 	<div class="grid grid-cols-2 mx-auto">
 		<div
-			class=" lg:-mt-10 z-10 text-right col-span-2 lg:col-span-1 mx-4 flex flex-col justify-center items-start"
+			class="mt-[200px] lg:mt-[85px] z-10 text-right col-span-2 lg:col-span-1 mx-4 flex flex-col justify-start items-start gap-4 lg:gap-8"
 		>
 			<h1
 				data-aos="fade-left"
@@ -18,7 +25,7 @@
 			<p
 				data-aos="fade-right"
 				data-aos-once="true"
-				class="bg-black/60 lg:bg-transparent text-white lg:text-theme-gray lg:text-[26px] lg:mt-16 mt-3 p-1"
+				class="bg-black/60 lg:bg-transparent text-white lg:text-theme-gray lg:text-[26px] p-1"
 			>
 				هذا هو المكان الذي يمكنك أن تجد فيه <br /> عقار الأحلام الذي تختاره دون ضغوط
 			</p>
