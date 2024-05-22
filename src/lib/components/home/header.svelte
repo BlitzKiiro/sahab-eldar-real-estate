06666aa130ccede2b5c47cf36cbfca69bf7da1fb
Ahmed Azmy <ahmedazmycs@protonmail.com>
2024-05-22 19:29:18 +0300

    home page header

---
+++ src/lib/components/home/header.svelte
@@ -0,0 +1,27 @@
+<script>
+	import bg from '$assets/imgs/header_bg2.png';
+</script>
+
+<div class="h-[770px] relative bg-[#EAF1FF] overflow-hidden flex justify-center" dir="rtl">
+	<img src={bg} alt="bg" class="lg:w-[38vw] select-none absolute left-0 object-cover h-full" />
+	<div class="grid grid-cols-2 mx-auto">
+		<div
+			class=" lg:-mt-10 z-10 text-right col-span-2 lg:col-span-1 mx-4 flex flex-col justify-center items-start"
+		>
+			<h1
+				data-aos="fade-left"
+				data-aos-once="true"
+				class="lg:text-8xl text-4xl lg:leading-[1.3] bg-black/60 lg:bg-transparent text-white lg:text-theme-black p-2"
+			>
+				أعثر علي <br class="hidden lg:block" /> عقار أحلامك
+			</h1>
+			<p
+				data-aos="fade-right"
+				data-aos-once="true"
+				class="bg-black/60 lg:bg-transparent text-white lg:text-theme-gray lg:text-[26px] lg:mt-16 mt-3 p-1"
+			>
+				هذا هو المكان الذي يمكنك أن تجد فيه <br /> عقار الأحلام الذي تختاره دون ضغوط
+			</p>
+		</div>
+	</div>
+</div>
