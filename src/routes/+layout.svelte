c6cacddb3920f911c59ec969ecc674b8ded61511
Ahmed Azmy <ahmedazmycs@protonmail.com>
2024-05-22 14:05:15 +0300

    aos install

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
+	// styles
 	import '$styles/globals.css';
-	import type { LayoutData } from './$types';
+	import 'aos/dist/aos.css';
 
+	// svelte
+	import type { LayoutData } from './$types';
+	import { onMount } from 'svelte';
+
+	// libs
+	import AOS from 'aos';
+
+	// components
 	import Navbar from '$/lib/components/layout/navbar.svelte';
 	import Footer from '$/lib/components/layout/footer.svelte';
 
 	// export let data: LayoutData;
+
+	onMount(() => {
+		AOS.init();
+	});
 </script>
 
 <Navbar />
