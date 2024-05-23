2f7f6289255901e4716634e283115a0191fc2b42
Ahmed Azmy <ahmedazmycs@protonmail.com>
2024-05-23 22:53:15 +0300

    about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
 	import Header from '$/lib/components/home/header.svelte';
+	import Results from '$/lib/components/home/results.svelte';
+	import About from '$/lib/components/home/about.svelte';
 </script>
 
 <Header />
+<Results />
+<About />
