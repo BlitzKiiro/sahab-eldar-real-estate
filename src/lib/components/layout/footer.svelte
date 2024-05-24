af16ebc7ac9acd5d5a817b1e96007af6146bce27
Ahmed Azmy <ahmedazmycs@protonmail.com>
2024-05-24 19:26:40 +0300

    footer

---
+++ src/lib/components/layout/footer.svelte
@@ -0,0 +1,6 @@
+<div
+	class="w-full flex-col flex justify-center items-center min-h-20 bg-theme-indigo text-white text-center"
+>
+	<p>© 2024 Sahab Aldar Real Estate</p>
+	<p>All rights reserved</p>
+</div>
